49ca8a602399d10073b790039517bc03619f2b15
Michal Cichra <michal@o2h.cz>
2014-10-01 16:21:19 +0200

    capistrano integration
    * create new Rake::Task that generates targets

--- lib/config_for/capistrano.rb
+++ lib/config_for/capistrano.rb
@@ -1,5 +1,95 @@
+require 'rake/tasklib'
+require 'pathname'
+require 'tempfile'
+require 'capistrano/dsl'
+require 'active_support/core_ext/hash/keys'
+
 module ConfigFor
   module Capistrano
 
+    # inspired by https://github.com/rspec/rspec-core/blob/6b7f55e4fb36226cf830983aab8da8308f641708/lib/rspec/core/rake_task.rb
+
+    class Task < ::Rake::TaskLib
+      include ::Capistrano::DSL
+
+      attr_reader :name
+
+      attr_accessor :folder
+
+      attr_reader :tempfile
+
+      def initialize(name, *args, &task_block)
+        @name = name
+        @folder = 'config'
+        @file = "#{name}.yml"
+        @roles = :all
+        @tempfile = ::Tempfile.new(@file)
+        @variable = "#{name}_yml".to_sym
+
+        desc "Generate #{name} uploader" unless ::Rake.application.last_comment
+        define(args, &task_block)
+      end
+
+
+      def path
+        ::File.join(@folder, @file)
+      end
+
+      def run_task
+        invoke("#{name}:upload")
+      end
+
+      def yaml
+        config = fetch(@variable, {})
+        config.deep_stringify_keys.to_yaml
+      end
+
+      private
+
+      def define(args, &task_block)
+        namespace name do
+          desc "Upload #{path} to remote servers"
+          task upload: path
+
+          desc "Remove #{path} from current and shared path"
+          task :remove do
+            files = []
+            files << shared_path.join(path)
+            files << current_path.join(path)
+
+            files.each do |file|
+              escaped = file.to_s.shellescape
+
+              on roles(@roles) do
+                if test "[ -e #{escaped} ]"
+                  execute :rm, escaped
+                end
+              end
+            end
+          end
+
+          desc "Reset #{name} config"
+          task reset: [:remove, :upload]
+
+          task :generate do
+            @tempfile.write(yaml)
+            @tempfile.close(false)
+          end
+        end
+
+        desc 'upload remote file'
+        remote_file path => @tempfile.path, roles: @roles
+        desc 'generate local file'
+        file @tempfile.path => "#{name}:generate"
+
+        desc "Generate #{path}"
+        task(name, *args) do |_, task_args|
+          task_block.call(*[self, task_args].slice(0, task_block.arity)) if task_block
+          run_task
+        end
+
+        before 'deploy:check:linked_files', @name
+      end
+    end
   end
 end
--- spec/capistrano_spec.rb
+++ spec/capistrano_spec.rb
@@ -3,3 +3,61 @@ require 'config_for/capistrano'
 RSpec.describe ConfigFor::Capistrano do
 
 end
+
+RSpec.describe ConfigFor::Capistrano::Task do
+  def invoke(task, *args)
+    rake_task(task).invoke(*args)
+  end
+
+  def rake_task(task)
+    Rake::Task[task]
+  end
+
+  before do
+    Rake.application = Rake::Application.new
+    Rake::Task.define_task('deploy:check:linked_files')
+  end
+
+  context 'database task' do
+    let!(:database_task) { described_class.new(:database) }
+    subject(:task) { database_task }
+
+    it { expect(task.path).to eq('config/database.yml') }
+
+    context 'with configuration' do
+      before{ stub_const('Capistrano::Configuration', configuration) }
+      let(:configuration) { double('configuration', env: env) }
+      let(:env) { { database_yml:  database_configuration } }
+      let(:database_configuration) { { production: { host: 'localhost' } } }
+
+      it { expect(task.yaml).to eq("---\nproduction:\n  host: localhost\n") }
+    end
+
+    let(:prerequisites) { subject.prerequisites }
+
+    context 'tempfile' do
+      subject { rake_task(task.tempfile.path) }
+      it { expect(prerequisites).to eq(['database:generate']) }
+    end
+
+    context 'remote_file' do
+      subject { rake_task('config/database.yml') }
+      it { expect(prerequisites).to eq([task.tempfile.path]) }
+    end
+
+    context 'upload' do
+      subject { rake_task('database:upload') }
+      it { expect(prerequisites).to eq(['config/database.yml']) }
+    end
+
+    context 'remove' do
+      subject { rake_task('database:remove') }
+      it { expect(prerequisites).to eq([]) }
+    end
+
+    context 'reset' do
+      subject { rake_task('database:reset') }
+      it { expect(prerequisites).to eq(%w[remove upload]) }
+    end
+  end
+end
