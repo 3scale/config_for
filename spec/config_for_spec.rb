0fc74df646c2fb0536fe57d2d378c55ff1d4bee2
Michal Cichra <michal@o2h.cz>
2014-11-03 12:07:43 +0100

    refactor config_for_spec

--- spec/config_for_spec.rb
+++ spec/config_for_spec.rb
@@ -2,13 +2,10 @@ require 'config_for'
 
 RSpec.describe ConfigFor do
   let(:env) { 'production' }
-  subject(:config) { config_for(self.class.metadata.fetch(:name)) }
 
-  def config_for(name)
-    described_class.load_config(fixtures_path, name, env)
-  end
+  shared_examples 'load_config' do
+    subject(:config) { config_for(self.class.metadata.fetch(:name)) }
 
-  context 'config', name: 'database' do
     it { is_expected.to eq('host' => 'localhost', 'port' => 3306) }
     it { is_expected.to include(port: 3306, host: 'localhost') }
 
@@ -18,23 +15,34 @@ RSpec.describe ConfigFor do
       it { is_expected.to_not be_empty }
     end
 
+    context 'unknown config', name: 'unknown' do
+      it do
+        expect{ subject }
+            .to raise_error(Errno::ENOENT, /^No such file or directory/)
+      end
+    end
+  end
+
+  context '.load_config', name: 'database' do
+
+    def config_for(name)
+      described_class.load_config(fixtures_path, name, env)
+    end
+
+    include_examples 'load_config'
+
     context 'with unknown env' do
       let(:env) { 'unknown' }
       it { is_expected.to be_empty }
     end
   end
 
-  context 'config', name: 'unknown' do
-    it do
-      expect{ subject }
-          .to raise_error(Errno::ENOENT, /^No such file or directory/)
-    end
-  end
-
-  context 'load_config!', name: 'database' do
+  context '.load_config!', name: 'database' do
     def config_for(name)
       described_class.load_config!(fixtures_path, name, env)
     end
+
+    include_examples 'load_config'
 
     context 'unknown env' do
       let(:env) { 'unknown' }
